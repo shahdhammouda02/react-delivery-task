caab60e9d8ea5066007d2a5e8a624dcf56410a90
Shahd Hammouda <99508416+shahdhammouda02@users.noreply.github.com>
2023-08-08 18:00:01 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect,createContext, useContext, } from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
@@ -18,75 +18,8 @@ import { CustomerContextProvider } from './components/CustomerContext';
 
 import './App.css';
 
-// const CustomerContext = createContext();
-
-// export const useCustomerContext = () => {
-//   return useContext(CustomerContext);
-// };
-// export const CustomerContextProvider = ({ customers, children, packages,invoice }) => {
-//   return (
-//     <CustomerContext.Provider value={{ customers, packages, invoice }}>
-//       {children}
-//     </CustomerContext.Provider>
-//   );
-// };
 function App() {
   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-  // console.log(data);
-//   const [appData, setAppData] = useState({ customers: [], packages: [], invoice: [] });
-//   const [invoices, setInvoices] = useState([]);
-//   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
-//   const [customerData, setCustomerData]=useState({});
-//   const [packageData, setPackageData]=useState({});
-
-
-//   useEffect(() => {
-//   fetch('/data.json')
-//     .then(response => response.json())
-//     .then((data) => {
-//       setAppData(data);
-//       // setInvoices(data);
-
-//       const customerDataObj = {};
-//       data.customers.forEach(item => {
-//         customerDataObj[item.id] = item.name;
-//       });
-//       setCustomerData(customerDataObj);
-
-//       const packageDataObj = {};
-//       data.packages.forEach(item => {
-//         const customerName = customerDataObj[item.customerid];
-//         packageDataObj[item.id] = {
-//           ...item,
-//           customerName: customerName || 'not Found',
-//         };
-//       });
-//       setPackageData(packageDataObj);
-//     })
-//     .catch(error => {
-//       console.error('Error fetching data:', error);
-//     });
-// }, []);
-
-  // console.log(appData.customers);
-  // console.log(appData.packages)
-
-  // const deleteCustomer=(customer_id)=>{
-  //   const updated=appData.customers.filter(customer=> customer.id !==customer_id);
-  //   setAppData({...appData, customers: updated});
-  // }
-
-  // const handleCreateInvoice = (customer_id) => {
-  //   const customer =appData.customers.find((c) => c.id === customer_id);
-  //   const customerPackages =appData.packages.filter((pkg) => pkg.customerid === customer_id);
-   
-  //   const invoice = {
-  //     customer_name: customer.name,
-  //     packages: customerPackages,
-  //   };
-  //   setInvoices([...invoices, invoice]);
-  // };
-
 
   return (
     <CustomerContextProvider>
@@ -114,25 +47,16 @@ function App() {
         <Switch>
         <Route exact path="/"></Route>
           <Route path="/customers">
-          {/* <CustomerContextProvider customers={appData.customers}> */}
-              <CustomerList 
-              />
-            {/* </CustomerContextProvider> */}
+              <CustomerList  />
           </Route>
           <Route path="/packages">
-            {/* <CustomerContextProvider packages={appData.packages}> */}
             <PackageList  />
-            {/* </CustomerContextProvider> */}
           </Route>
           <Route path="/invoices">
-            {/* <CustomerContextProvider > */}
             <InvoiceList/>
-            {/* </CustomerContextProvider> */}
           </Route>
         <Route path="/invoice/:customer_id">
-          {/* <CustomerContextProvider customers={appData.customers} packages={appData.packages}> */}
                <Invoice />
-            {/* </CustomerContextProvider> */}
         </Route>
         
         </Switch>
