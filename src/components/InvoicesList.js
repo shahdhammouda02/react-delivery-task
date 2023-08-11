8df6d4b18085faba8843f7f232a295ca46df9611
Shahd Hammouda <99508416+shahdhammouda02@users.noreply.github.com>
2023-08-11 16:50:06 +0300

    Update InvoicesList.js

--- src/components/InvoicesList.js
+++ src/components/InvoicesList.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React ,{useEffect} from 'react';
 import TableContainer from '@mui/material/TableContainer';
 import Table from '@mui/material/Table';
 import TableHead from '@mui/material/TableHead';
@@ -7,20 +7,13 @@ import TableCell from '@mui/material/TableCell';
 import TableBody from '@mui/material/TableBody';
 import Paper from '@mui/material/Paper';
 import { useCustomerContext } from './CustomerContext';
-import { useEffect, useState } from 'react';
 
 const InvoiceList = () => {
-  const [invoices, setInvoices] = useState([]);
-  const { getInvoiceList } = useCustomerContext();
-
-  useEffect(() => {
-    const fetchInvoices = async () => {
-      const invoiceListWithTotals = await getInvoiceList();
-      setInvoices(invoiceListWithTotals);
-    };
-
-    fetchInvoices();
-  }, [getInvoiceList]);
+ const { invoiceDataList, generateInvoiceDataList } = useCustomerContext();
+   
+    useEffect(() => {
+      generateInvoiceDataList();
+    }, [generateInvoiceDataList]);
 
   return (
     <TableContainer component={Paper}>
@@ -32,12 +25,12 @@ const InvoiceList = () => {
             <TableCell>Total Weight</TableCell>
           </TableRow>
         </TableHead>
-        <TableBody>
-          {invoices.map((invoice, index) => (
+       <TableBody>
+          {invoiceDataList.map((invoiceData, index) => (
             <TableRow key={index}>
-              <TableCell>{invoice.customer_name}</TableCell>
-              <TableCell>{invoice.totalpkgPrice}</TableCell>
-              <TableCell>{invoice.totalpkgWeight.toFixed(2)} kg</TableCell>
+              <TableCell>{invoiceData.customerName}</TableCell>
+              <TableCell>{invoiceData.totalpkgPrice}</TableCell>
+              <TableCell>{invoiceData.totalpkgWeight.toFixed(2)} kg</TableCell>
             </TableRow>
           ))}
         </TableBody>
