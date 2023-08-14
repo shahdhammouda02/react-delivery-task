f8f6ba980a369d70ded92e0735d035d53af8dcd6
Shahd Hammouda <99508416+shahdhammouda02@users.noreply.github.com>
2023-08-14 00:55:23 -0700

    Add files via upload

---
+++ src/components/SnackbarComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Snackbar from '@mui/material/Snackbar';
+import MuiAlert from '@mui/material/Alert';
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+    return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+  });
+  
+  const SnackbarComponent = ({ open, message, severity, onClose }) => {
+    return (
+      <Snackbar open={open} autoHideDuration={3000} onClose={onClose}>
+        <Alert onClose={onClose} severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
+    );
+  };
+  
+  export default SnackbarComponent;
